37ec0dd836393ffc4b678396a45c14d71ef2d94b
Franck-Aimé <franckkoukougnnon@yahoo.fr>
2021-04-16 11:15:15 +0200

    1 er test

---
+++ cours_1/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+16/04/2021
+Author: ib
+"""
---
+++ cours_1/exo_1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+16/04/2021
+Author: ib
+"""
+temps = 6.892
+distance = 19.7
+
+vitesse = distance / temps
+
+vitesse_arrondi = round(vitesse, 2)
+
+print(vitesse_arrondi)
